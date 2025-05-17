c0c155d45eb5946d230a43bff307ddf89bbedbe9
Toasting Machine <Toastgeraet@users.noreply.github.com>
2025-05-17 20:27:41 +0200

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { createNextcloudConfig } from '@nextcloud/vite-config'
 import { fileURLToPath, URL } from 'node:url'
 
-// https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(createNextcloudConfig({
+  appName: 'bloodmeetup',
   plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   }
-})
+}))
